e30bf093baa569a36c925fa503cece71a24024e2
Marcos Botene <marcosjbotene@gmail.com>
2021-02-28 19:15:37 -0300

    Button created and CSS adjustments

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,13 +1,19 @@
+import Head from 'next/head';
+
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 
 import styles from '../styles/pages/Home.module.css';
-import { Countdown } from '../components/Countdown';
 
 const Home = () => {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | Move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
