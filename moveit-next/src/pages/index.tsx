999dbe3ec619a0ce2573a274692cbf89987f50b0
Marcos Botene <marcosjbotene@gmail.com>
2021-02-28 18:03:10 -0300

    Home CSS created

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,13 +1,15 @@
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 
+import styles from '../styles/pages/Home.module.css';
+
 const Home = () => {
   return (
-    <div className='container'>
+    <div className={styles.container}>
       <ExperienceBar />
 
       <section>
-        <div>
+        <div className={styles.left}>
           <Profile />
         </div>
         <div></div>
