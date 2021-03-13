9293733a9ba2688b8ca686390eb15039c01aa8df
Marcos Botene <marcosjbotene@gmail.com>
2021-03-12 23:10:12 -0300

    Created Challenges Types

--- moveit-next/src/@types/index.d.ts
+++ moveit-next/src/@types/index.d.ts
@@ -2,6 +2,14 @@ interface ChallengesContextData {
   level: number;
   currentExperience: number;
   challengesCompleted: number;
+  activeChallenge: Challenge | null;
   levelUp: () => void;
   startNewChallenge: () => void;
+  resetChallenge: () => void;
+}
+
+interface Challenge {
+  type: string;
+  description: string;
+  amount: number;
 }
