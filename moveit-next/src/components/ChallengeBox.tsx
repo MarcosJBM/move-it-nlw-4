1f5ebbef06bdf0e1048d372551ccdebf9f5ecd3f
Marcos Botene <marcosjbotene@gmail.com>
2021-03-15 21:18:28 -0300

    Using reset countdown in Challenge Box.

--- moveit-next/src/components/ChallengeBox.tsx
+++ moveit-next/src/components/ChallengeBox.tsx
@@ -1,11 +1,25 @@
 import { useContext } from 'react';
+
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
+
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export const ChallengeBox = () => {
-  const { activeChallenge, resetChallenge, completeChallenge } = useContext(
+  const { activeChallenge, completeChallenge, resetChallenge } = useContext(
     ChallengesContext
   );
+  const { resetCountdown } = useContext(CountdownContext);
+
+  function handleChallengeSucceeded() {
+    completeChallenge();
+    resetCountdown();
+  }
+
+  function handleChallengeFailed() {
+    resetChallenge();
+    resetCountdown();
+  }
 
   return (
     <div className={styles.challengeBoxContainer}>
@@ -23,14 +37,14 @@ export const ChallengeBox = () => {
             <button
               type='button'
               className={styles.challengeFailedButton}
-              onClick={resetChallenge}
+              onClick={handleChallengeFailed}
             >
               Falhei
             </button>
             <button
               type='button'
               className={styles.challengeSucceededButton}
-              onClick={completeChallenge}
+              onClick={handleChallengeSucceeded}
             >
               Completei
             </button>
