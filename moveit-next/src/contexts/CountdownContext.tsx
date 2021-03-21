914c27a670ce89725c3254b4a78d7f5ad771517e
Marcos Botene <marcosjbotene@gmail.com>
2021-03-20 23:54:18 -0300

    Added Vercel in gitignore.

--- moveit-next/src/contexts/CountdownContext.tsx
+++ moveit-next/src/contexts/CountdownContext.tsx
@@ -9,7 +9,7 @@ let countdowmnTimeout: NodeJS.Timeout;
 export const CountdownProvider: React.FC = ({ children }) => {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -24,7 +24,7 @@ export const CountdownProvider: React.FC = ({ children }) => {
     clearTimeout(countdowmnTimeout);
     setIsActive(false);
     setHasFinished(false);
-    setTime(0.1 * 60);
+    setTime(25 * 60);
   }
 
   useEffect(() => {
